1b4c9643d357f1400b6ca96d446c6f8c78ba3d73
Cuong Ngo <dinhcuongngo@gmail.com>
2025-08-05 18:07:05 +0700

    feat: multi locales

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,7 +10,7 @@
           @click="state.showLocales = !state.showLocales"
           aria-label="Change Language"
         >
-          <img :src="getFlagPath('vi.svg')" alt="Vi" width="32" height="32">
+          <img :src="getFlagPath(`${locale}.svg`)" alt="Vi" width="32" height="32">
         </button>
 
         <Transition name="locale-menu">
@@ -18,12 +18,14 @@
             v-if="state.showLocales"
             class="absolute right-0 flex space-x-2 px-2 bg-white shadow rounded-lg"
           >
-            <button class=" w-8 h-8 ">
-              <img src="/flags/en.svg" width="32" height="32"/>
-            </button>
-            <button class=" w-8 h-8 ">
-              <img src="/flags/en.svg" width="32" height="32"/>
-            </button>
+            <template v-for="loc in locales" :key="loc.code">
+              <button
+                class="w-8 h-8"
+                @click="setLocale(loc.code)"
+              >
+                <img :src="getFlagPath(`${loc.code}.svg`)" :alt="loc.name" width="32" height="32"/>
+              </button>
+            </template>
           </div>
         </Transition>
 
@@ -32,9 +34,9 @@
 
     <section id="hero" class="container max-auto items-center grid grid-cols-1 md:grid-cols-2">
       <div class="text-center text-white px-4">
-        <h2 class="text-4xl md:text-8xl font-extrabold my-4">Massage Đà Nẵng</h2>
-        <p class="text-lg mb-10">Trải nghiệm dịch vụ massage tại nhà chuyên nghiệp và thư giãn.</p>
-        <a href="#" class="bg-primary font-bold py-5 px-10 rounded-lg">Xem Dịch Vụ</a>
+        <h2 class="text-4xl md:text-8xl font-extrabold my-4">{{ $t('banner_title') }}</h2>
+        <p class="text-lg mb-10">{{ $t('banner_description') }}</p>
+        <a href="#" class="bg-primary font-bold py-5 px-10 rounded-lg">{{ $t('book_now') }}</a>
       </div>
 
       <div class="flex justify-center md:justify-end">
@@ -47,33 +49,19 @@
     <!-- SERVICES -->
     <section id="services" class="py-16 bg-gray-100">
       <div class="container mx-auto my-10 px-4">
-        <h2 class="text-3xl font-bold text-center mb-8">Liệu Trình Chăm Sóc</h2>
-        <p class="mt-4 mb-16 text-center">Chúng tôi cung cấp các liệu trình đa dạng, phù hợp với mọi nhu cầu thư giãn và trị liệu của bạn.</p>
+        <h2 class="text-3xl font-bold text-center mb-8">{{ $t('services.title') }}</h2>
+        <p class="mt-4 mb-16 text-center">{{ $t('services.description') }}</p>
         
         <div class="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8">
-          <div class="bg-white rounded-lg shadow text-center hover:shadow-lg hover:scale-105 transition duration-400">
-            <img :src="getImagePath('service-body.jpg')" alt="Dịch vụ 1" class="rounded-t-lg">
-            <div class="p-5">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">Massage toàn thân</h5>
-              <p>Liệu pháp massage toàn thân giúp giảm căng thẳng, cải thiện tuần hoàn máu và mang lại cảm giác thư giãn sâu.</p>
+          <template v-for="service in $tm('services.list')">
+            <div class="bg-white rounded-lg shadow text-center hover:shadow-lg hover:scale-105 transition duration-400">
+              <img :src="getImagePath($rt(service.image))" :alt="$rt(service.title)" class="rounded-t-lg">
+              <div class="p-5">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">{{ $rt(service.title) }}</h5>
+                <p>{{ $rt(service.description) }}</p>
+              </div>
             </div>
-          </div>
-
-          <div class="bg-white rounded-lg shadow text-center">
-            <img :src="getImagePath('service-body.jpg')" alt="Dịch vụ 1" class="rounded-t-lg">
-            <div class="p-5">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">Massage toàn thân</h5>
-              <p>Liệu pháp massage toàn thân giúp giảm căng thẳng, cải thiện tuần hoàn máu và mang lại cảm giác thư giãn sâu.</p>
-            </div>
-          </div>
-
-          <div class="bg-white rounded-lg shadow text-center">
-            <img :src="getImagePath('service-body.jpg')" alt="Dịch vụ 1" class="rounded-t-lg">
-            <div class="p-5">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">Massage toàn thân</h5>
-              <p>Liệu pháp massage toàn thân giúp giảm căng thẳng, cải thiện tuần hoàn máu và mang lại cảm giác thư giãn sâu.</p>
-            </div>
-          </div>
+          </template>
         </div>
       </div>
     </section>
@@ -81,29 +69,24 @@
     <!-- PRICES -->
     <section id="prices">
       <div class="container mx-auto my-10 px-4">
-        <h2 class="text-3xl font-bold text-center mb-8">Chi Phí Dịch Vụ</h2>
-        <p class="mt-4 mb-16 text-center">Các gói dịch vụ được thiết kế linh hoạt, đảm bảo chất lượng với chi phí hợp lý nhất.</p>
+        <h2 class="text-3xl font-bold text-center mb-8">{{ $t('prices.title') }}</h2>
+        <p class="mt-4 mb-16 text-center">{{ $t('prices.description') }}</p>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-12 max-w-4xl mx-auto">
-          <div class="bg-white rounded-lg shadow text-center hover:shadow-lg hover:scale-105 transition duration-400">
-            <div class="p-5">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">Gói Cơ Bản</h5>
-              <ul>
-                <li class="flex justify-between items-center mb-2">
-                  <span>60 phút</span>
-                  <span class="font-semibold">500k</span>
-                </li>
-              </ul>
+          <template v-for="list in $tm('prices.list')">
+            <div class="bg-white rounded-lg shadow text-center hover:shadow-lg hover:scale-105 transition duration-400">
+              <div class="p-5">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">{{ $rt(list.name) }}</h5>
+                <ul>
+                  <li v-for="item in list.items" class="flex justify-between items-center py-2 border-b last:border-0 border-gray-200">
+                    <span>{{ $rt(item.duration) }}</span>
+                    <span class="font-semibold">{{ $rt(item.price) }}</span>
+                  </li>
+                </ul>
+                <p class="text-gray-500" v-if="list.note">{{ $rt(list.note) }}</p>
+              </div>
             </div>
-          </div>
-
-          <div class="bg-white rounded-lg shadow text-center">
-            <div class="p-5">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary">Gói Nâng Cao</h5>
-              <p class="text-xl font-semibold mb-4">800.000 VNĐ</p>
-              <p>Massage toàn thân 90 phút, bao gồm dầu massage tự nhiên và liệu pháp đá nóng.</p>
-            </div>
-          </div>
+          </template>
         </div>
       </div>
     </section>
@@ -112,9 +95,9 @@
     <section id="aboutUs" class="py-10">
       <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-4">
         <div class="text-white">
-          <h2 class="text-3xl font-bold text-center mb-8">Về Chúng Tôi</h2>
-          <p class="mb-4">HomeMassageDanang là đơn vị tiên phong trong lĩnh vực massage tại nhà ở Đà Nẵng. Với đội ngũ kỹ thuật viên được đào tạo chuyên sâu và kinh nghiệm thực tế, chúng tôi cam kết mang đến sự hài lòng tối đa cho khách hàng. Chúng tôi đặt mục tiêu giúp bạn thư giãn sau những ngày làm việc căng thẳng, tái tạo năng lượng và cải thiện sức khỏe toàn diện. HomeMassageDanang - Chăm sóc sức khỏe ngay tại ngôi nhà của bạn.</p>
-          <p class="text-center">Liên hệ với chúng tôi để được tư vấn và đặt lịch hẹn.</p>
+          <h2 class="text-3xl font-bold text-center mb-8">{{ $t('about.title') }}</h2>
+          <p class="mb-4">{{ $t('about.description') }}</p>
+          <p class="text-center">{{ $t('about.mission') }}</p>
         </div>
         <img
           :src="getImagePath('about-us.jpg')" alt="Về Chúng Tôi" width="736" height="617"
@@ -126,43 +109,22 @@
     <!-- CUSTOMER's FEEDBACKs -->
     <section id="testimonials" class="py-16">
       <div class="container mx-auto px-4">
-        <h2 class="text-3xl font-bold text-center mb-8">Khách Hàng Nói Gì Về Chúng Tôi</h2>
-        <p class="mt-4 mb-16 text-center">Chúng tôi tự hào nhận được nhiều phản hồi tích cực từ khách hàng. Dưới đây là một số đánh giá tiêu biểu.</p>
+        <h2 class="text-3xl font-bold text-center mb-8">{{ $t('feedbacks.title') }}</h2>
+        <p class="mt-4 mb-16 text-center">{{ $t('feedbacks.description') }}</p>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div class="rounded-lg shadow p-6">
-            <p class="text-gray-700 mb-4">"Dịch vụ tuyệt vời! Kỹ thuật viên rất chuyên nghiệp và thân thiện. Tôi cảm thấy thư giãn hoàn toàn sau buổi massage."</p>
-            <div class="flex items-center">
-              <img src="https://randomuser.me/api/portraits/lego/3.jpg" alt="Khách hàng A" class="w-12 h-12 rounded-full mr-3">
-              <div>
-                <p class="font-semibold">Nguyễn Văn A</p>
-                <p class="text-gray-500">Đà Nẵng</p>
+          <template v-for="testimonial in $tm('feedbacks.testimonials')">
+            <div class="rounded-lg shadow p-6">
+              <p class="text-gray-700 mb-4 h-10">{{ $rt(testimonial.feedback) }}</p>
+              <div class="flex items-center">
+                <img src="https://randomuser.me/api/portraits/lego/3.jpg" :alt="$rt(testimonial.name)" class="w-12 h-12 rounded-full mr-3">
+                <div>
+                  <p class="font-semibold">{{ $rt(testimonial.name) }}</p>
+                  <p class="text-gray-500">{{ $rt(testimonial.from) }}</p>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div class=" rounded-lg shadow p-6">
-            <p class="text-gray-700 mb-4">"Tôi đã sử dụng dịch vụ nhiều lần và luôn hài lòng. Chất lượng dịch vụ rất tốt, giá cả hợp lý."</p>
-                        <div class="flex items-center">
-              <img src="https://randomuser.me/api/portraits/lego/3.jpg" alt="Khách hàng A" class="w-12 h-12 rounded-full mr-3">
-              <div>
-                <p class="font-semibold">Nguyễn Văn A</p>
-                <p class="text-gray-500">Đà Nẵng</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="rounded-lg shadow p-6">
-            <p class="text-gray-700 mb-4">"Rất hài lòng với dịch vụ của HomeMassageDanang. Tôi sẽ giới thiệu cho bạn bè và người thân."</p>
-            <div class="flex items-center">
-              <img src="https://randomuser.me/api/portraits/lego/3.jpg" alt="Khách hàng A" class="w-12 h-12 rounded-full mr-3">
-              <div>
-                <p class="font-semibold">Nguyễn Văn A</p>
-                <p class="text-gray-500">Đà Nẵng</p>
-              </div>
-            </div>
-          </div>
-
+          </template>
         </div>
       </div>
     </section>
@@ -178,7 +140,7 @@
         </div>
 
         <div class="text-center md:text-right order-first md:order-last mb-10">
-          <p class="mb-4">Liên hệ:</p>
+          <p class="mb-4">{{ $t('contact') }}</p>
           <div class="flex justify-center md:justify-end space-x-6">
             <a href="#" class="text-white hover:text-gray-400">
               <img :src="getLogoPath('facebook.svg')" alt="Facebook" width="24" height="24">
@@ -202,6 +164,8 @@
 const state = reactive({
   showLocales: false
 });
+
+const { locales, locale, setLocale } = useI18n();
 </script>
 <style>
 #header {
